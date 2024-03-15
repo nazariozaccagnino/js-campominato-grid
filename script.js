f705d68b9d86b1ff9e1c33f98dd82529a4abc50f
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-15 14:35:35 +0100

    created the first square with js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let button = document.getElementById('startbutton')
 button.addEventListener('click', function() {
+    const littleSquare = document.createElement("div");
+    littleSquare.className = 'littlesquare'
+    document.getElementById('mainsquare').appendChild(littleSquare)
     
 })
