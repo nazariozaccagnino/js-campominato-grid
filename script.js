86982d7b7d8466a079f57e04d28bfedae7f70d9b
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-15 17:28:41 +0100

    bonus started

--- script.js
+++ script.js
@@ -9,23 +9,61 @@ let button = document.getElementById('startbutton')
 button.addEventListener('click', function(){
     document.getElementById('mainsquare').classList.toggle('d-none')
     
-})   
-    let mainsquare = document.getElementById('mainsquare')
+    let choice = parseInt(document.getElementById("choice").value)
+    console.log(choice)
 
-    for (let i = 1; i<= 100; i++){
-        let littlesq = createLittleSq();
-        mainsquare.appendChild(littlesq);
-        littlesq.addEventListener('click', function(){
-            littlesq.classList.add('squareon')
-            const node = document.createElement("div");
-            const textnode = document.createTextNode(i);
-            node.appendChild(textnode)
-            console.log(node);
-            littlesq.appendChild(node)
-            console.log('Hai cliccato la cella n.', i)
-            
-        }, {once : true})
+    let mainsquare = document.getElementById('mainsquare')
+    if(choice ===1){
+        for (let i = 1; i<= 100; i++){
+            let littlesq = createLittleSq();
+            mainsquare.appendChild(littlesq);
+            littlesq.addEventListener('click', function(){
+                littlesq.classList.add('squareon')
+                const node = document.createElement("div");
+                const textnode = document.createTextNode(i);
+                node.appendChild(textnode)
+                littlesq.appendChild(node)
+                console.log('Hai cliccato la cella n.', i)
+                
+            }, {once : true})
+        }
+    } if (choice === 2){
+        for (let i = 1; i<= 81; i++){
+            let littlesq = createLittleSq();
+            mainsquare.appendChild(littlesq);
+            littlesq.addEventListener('click', function(){
+                littlesq.classList.add('squareon')
+                const node = document.createElement("div");
+                const textnode = document.createTextNode(i);
+                node.appendChild(textnode)
+                littlesq.appendChild(node)
+                console.log('Hai cliccato la cella n.', i)
+                
+            }, {once : true})
+        }
+    } else {
+        for (let i = 1; i<= 49; i++){
+            let littlesq = createLittleSq();
+            mainsquare.appendChild(littlesq);
+            littlesq.addEventListener('click', function(){
+                littlesq.classList.add('squareon')
+                const node = document.createElement("div");
+                const textnode = document.createTextNode(i);
+                node.appendChild(textnode)
+                littlesq.appendChild(node)
+                console.log('Hai cliccato la cella n.', i)
+                
+            }, {once : true})
+        }
     }
+    
+
+
+
+}, {once : true})   
+    
+    
+        
     
     
 
